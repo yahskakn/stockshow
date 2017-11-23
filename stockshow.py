ea5fd94668f97f9a4293807ba2e865cad7e0cce7
Akshaya Sharma <yahskakn@gmail.com>
2017-11-22 16:53:02 -0800

    Adding help message and ability to read stock symbols from [file|command line list]. Also adding an example file

--- stockshow.py
+++ stockshow.py
@@ -4,16 +4,37 @@ from __future__ import print_function
 import json
 import requests
 import prettytable
-import time
+import os.path
 import sys
+import argparse
+import pdb
 
-def getCompanyList():
-    #return hard dumb list for now
-    return ['NFLX', 'NVDA']
+def getCompanyList(args):
+    if args.file is not None:
+        if os.path.exists(args.file):
+            with open(args.file) as f:
+                content = f.readlines()
+            content = [company.strip() for company in content]
+            return content
+        else:
+            #return example list
+            return ['NFLX', 'NVDA']
+    elif args.list is not None:
+        return args.list
+
+def parseArgs():
+    parser=argparse.ArgumentParser(
+            description='''Script to print stock data using google finance''',
+            epilog="""Happy Trading"""
+    )
+    parser.add_argument('-f', '--file', type=str, help='provide filename with list of stock symbols')
+    parser.add_argument('-l', '--list', nargs='+', help='provide list of stock symbols on the terminal')
+    return parser.parse_args()
 
 def main():
-    #Read interest file or user input
-    company_list = getCompanyList()
+    args = parseArgs()
+
+    company_list = getCompanyList(args)
 
     if company_list:
         tbl = prettytable.PrettyTable(["Company", "Last Price", "Opening", "Change"])
@@ -23,8 +44,9 @@ def main():
         rsp = requests.get(request)
 
         if rsp.status_code in (200,0):
-            fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
-            tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], fin_data['c']])
+            if "iid" in rsp.content:
+                fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+                tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], fin_data['c']])
     print(tbl)
 
 main()
