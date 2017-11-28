f3c19c4bf7033f487dbd2ee9de5bfe8d98301903
Akshaya Sharma <yahskakn@gmail.com>
2017-11-28 15:44:34 -0800

    1) Adding default prints for when no args are given 2) Adding ncurses to display in window with refresh rate of 0.2 seconds 3) Adding percentage change in stocks along with $ value

--- stockshow.py
+++ stockshow.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python#
 #Script to grab stock data and display
 from __future__ import print_function
+import curses
+from curses import wrapper
 import json
 import requests
 import prettytable
 import os.path
-import sys
+#import sys
 import argparse
 import pdb
+import time
+
+#Sleep time in seconds
+SLEEP_TIME = 0.2
 
 def getCompanyList(args):
     if args.file is not None:
@@ -21,6 +27,8 @@ def getCompanyList(args):
             return ['NFLX', 'NVDA']
     elif args.list is not None:
         return args.list
+    else:
+        return ['NFLX', 'NVDA']
 
 def parseArgs():
     parser=argparse.ArgumentParser(
@@ -31,13 +39,13 @@ def parseArgs():
     parser.add_argument('-l', '--list', nargs='+', help='provide list of stock symbols on the terminal')
     return parser.parse_args()
 
-def main():
+def getTable():
     args = parseArgs()
 
     company_list = getCompanyList(args)
 
     if company_list:
-        tbl = prettytable.PrettyTable(["Company", "Last Price", "Opening", "Change"])
+        tbl = prettytable.PrettyTable(["Company", "Last Price", "Opening", "Change(%)"])
     #iterate through list and get stock data
     for company in company_list:
         request = 'https://finance.google.com/finance?q='+company+'&output=json'
@@ -46,7 +54,20 @@ def main():
         if rsp.status_code in (200,0):
             if "iid" in rsp.content:
                 fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
-                tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], fin_data['c']])
-    print(tbl)
+                tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], str(fin_data['c']+'('+fin_data['cp']+')')])
 
-main()
+    return str(tbl)
+
+def main(stdscr):
+    win = curses.initscr()
+    win.nodelay(1)
+    while(True):
+        if (win.getch() == curses.KEY_F1):
+            curses.endwin()
+            return
+        win.clear()
+        win.addstr(getTable())
+        win.refresh()
+        time.sleep(SLEEP_TIME)
+
+wrapper(main)
