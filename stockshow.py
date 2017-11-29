80e1ab294f9fe9a0729de21609ae343d07f76695
Akshaya Sharma <yahskakn@gmail.com>
2017-11-29 15:09:56 -0800

    Added capability to get data from ALPHA VANTAGE for speed, but google seems to be doing better. Will leave the code be anyway. In case you want to use Alpha vantage change source to ALPHA and get a API key from their site and add it in the global variable

--- stockshow.py
+++ stockshow.py
@@ -11,9 +11,17 @@ import os.path
 import argparse
 import pdb
 import time
+import datetime
+from alpha_vantage.timeseries import TimeSeries
 
 #Sleep time in seconds
-SLEEP_TIME = 0.2
+SLEEP_TIME = 1.7
+API_KEY = ' '
+
+ts = TimeSeries(key=API_KEY)
+now = datetime.datetime.now()
+date = now.strftime("%Y-%m-%d")
+source = 'GOOGLE'
 
 def getCompanyList(args):
     if args.file is not None:
@@ -39,6 +47,13 @@ def parseArgs():
     parser.add_argument('-l', '--list', nargs='+', help='provide list of stock symbols on the terminal')
     return parser.parse_args()
 
+def getAlphaData(fin_data):
+    cl = fin_data[date]['4. close']
+    op = fin_data[date]['1. open']
+    ch = float(cl) - float(op)
+    chp = (ch / float(op) ) * 100
+    return str(cl), str(op), str(ch), str(round(chp,2))
+
 def getTable():
     args = parseArgs()
 
@@ -46,15 +61,21 @@ def getTable():
 
     if company_list:
         tbl = prettytable.PrettyTable(["Company", "Last Price", "Opening", "Change(%)"])
+
     #iterate through list and get stock data
     for company in company_list:
-        request = 'https://finance.google.com/finance?q='+company+'&output=json'
-        rsp = requests.get(request)
+        if source == 'GOOGLE':
+            request = 'https://finance.google.com/finance?q='+company+'&output=json'
+            rsp = requests.get(request)
 
-        if rsp.status_code in (200,0):
-            if "iid" in rsp.content:
-                fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
-                tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], str(fin_data['c']+'('+fin_data['cp']+')')])
+            if rsp.status_code in (200,0):
+                if "iid" in rsp.content:
+                    fin_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+                    tbl.add_row([fin_data['name'], fin_data['l'], fin_data['op'], str(fin_data['c']+'('+fin_data['cp']+')')])
+        elif source == 'ALPHA':
+            fin_data, _ = ts.get_daily(company, outputsize='compact')
+            cl, op, ch, chp = getAlphaData(fin_data)
+            tbl.add_row([company, cl, op, str(ch+'('+chp+')')])
 
     return str(tbl)
 
